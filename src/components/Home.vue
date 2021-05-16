46d944252833c2435a6b490a9c46123cf2fdb8bd
euphonium1998 <814811223@qq.com>
2021-05-16 23:10:18 +0800

    user part fininshed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -24,6 +24,24 @@
               <el-dropdown-item command="modifyPassword">修改密码</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
+
+          <el-dialog title="修改密码" :visible.sync="modifyPasswordVisible" width="50%" :before-close="handleClose">
+            <el-form ref="form" :model="password" label-width="120px">
+              <el-form-item label="输入旧密码">
+                <el-input v-model="password.oldPassword" clearable show-password></el-input>
+              </el-form-item>
+              <el-form-item label="输入新密码">
+                <el-input v-model="password.newPassword" clearable show-password></el-input>
+              </el-form-item>
+              <el-form-item label="再次输入新密码">
+                <el-input v-model="password.newPassword2" clearable show-password></el-input>
+              </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+              <el-button @click="modifyPasswordVisible = false">取 消</el-button>
+              <el-button type="primary" @click=modifyPassword>确 定</el-button>
+            </span>
+          </el-dialog>
         </el-header>
 
         <el-main>
@@ -51,6 +69,7 @@ import store from '@/store/index'
 import Monaco from "@/components/Monaco";
 import {BASE_URL} from "@/const";
 import {SET_EDITOR_CONTENT} from "@/store/mutation-types";
+import {SET_USER_INFO} from "../store/mutation-types";
 
 window.onload = function() {
   document.getElementById("container").style.height = (screen.height - 250).toString() + "px";
@@ -64,6 +83,12 @@ export default {
       fileUploadUrl: BASE_URL + 'file/upload',
       codeReviewDialogVisible: false,
       codeReviewMsgList: ['正在进行代码审核，请稍等...'],
+      modifyPasswordVisible: false,
+      password: {
+        oldPassword: '',
+        newPassword: '',
+        newPassword2: '',
+      }
     }
   },
   components: {
@@ -102,14 +127,57 @@ export default {
       })
     },
     modifyPassword() {
-      console.log('modify');
+      // console.log('modify');
+      if (this.password.oldPassword !== store.state.userInfo.password) {
+        this.$notify({
+          title: '警告',
+          message: '输入旧密码错误',
+          type: 'warning'
+        });
+        this.clearPassword()
+      } else if (this.password.newPassword !== this.password.newPassword2) {
+        this.$notify({
+          title: '警告',
+          message: '输入密码前后不一致',
+          type: 'warning'
+        });
+        this.clearPassword()
+      } else {
+        let account = store.state.userInfo.account
+        let data = {"account": account, "password": this.password.newPassword}
+        axios.post(BASE_URL + 'modifyPassword', data)
+          .then(res => {
+            if (res.data.status === 1) {
+              this.$message('数据库错误，修改密码失败')
+            } else {
+              sessionStorage.setItem('user_info', JSON.stringify(res.data))
+              store.commit(SET_USER_INFO, res.data)
+              this.$message('修改密码成功!')
+            }
+          })
+        this.clearPassword()
+        this.modifyPasswordVisible = false
+      }
+
     },
     handleCommand(command) {
       if (command === 'logout') {
         this.logout()
       } else if (command === 'modifyPassword') {
-        this.modifyPassword()
+        this.modifyPasswordVisible = true
       }
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+    },
+    clearPassword() {
+      this.password.oldPassword = ''
+      this.password.newPassword = ''
+      this.password.newPassword2 = ''
     }
   }
 }
@@ -136,4 +204,6 @@ export default {
   color: #333;
   line-height: 60px;
 }
+
+
 </style>
